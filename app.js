c6d117160c1ad15e85a6937e419bb3e77dca68af
waey <hesakibooks@gmail.com>
2024-06-22 11:51:14 +0100

    button event listener display fix

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const displayContent = document.getElementById("reorderdisplay");
 const previousNumber = document.getElementById("previousNumber");
 
 const currentNumber = document.getElementById("currentNumber");
+
+const buttons = document.querySelectorAll(".buttons");
+
+buttons.forEach((button) =>
+	button.addEventListener("click", function () {
+		currentNumber.textContent = currentNumber.textContent + button.textContent;
+		if (
+			button.textContent === "+" ||
+			button.textContent === "X" ||
+			button.textContent === "-" ||
+			button.textContent === "/"
+		) {
+			previousNumber.textContent = currentNumber.textContent;
+			currentNumber.textContent = "";
+		}
+	})
+);
 clearBtn.addEventListener("click", function (e) {
-	previousNumber.textContent = "";
-	displayContent.textContent = "";
+	location.reload();
 });
