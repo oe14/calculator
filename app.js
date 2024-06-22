2849f9681074470d02dfe1fab0615b87d8910965
waey <hesakibooks@gmail.com>
2024-06-22 11:25:46 +0100

    started implementing clear btn

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-function addition(a, b) {
-   return a + b;
-}
-function multiplication(a, b) {
-   return a * b;
-}
-function division(a, b) {
-   return a - b;
-}
+const clearBtn = document.getElementById("clearBtn");
+const displayContent = document.getElementById("reorderdisplay");
+const previousNumber = document.getElementById("previousNumber");
 
+const currentNumber = document.getElementById("currentNumber");
+clearBtn.addEventListener("click", function (e) {
+	previousNumber.textContent = "";
+	displayContent.textContent = "";
+});
